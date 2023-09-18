8bfbec7e16a5c108ea435d592955f71cfb873595
PedroCavataio <pedrocavataio@gmail.com>
2023-09-18 15:44:05 -0300

    trabajo

--- pfc_portfolio/src/trabajo/trabajo.jsx
+++ pfc_portfolio/src/trabajo/trabajo.jsx
@@ -1,15 +1,101 @@
 import React from "react";
 import "./trabajo.styles.css";
-
+import Tecnocasa from "../assets/tecnocasa.png";
 
 const trabajo = () => {
+  return (
+    <div className="trabajo-container">
+      <div className="section">
+        <div className="proyecto">
+          <div className="contAbout-texto">
+            <p>
+              <span class="job-role">. Full Stack Web Developer</span>
+              <br />
+              <span class="company-name">. Henry</span>
+              <br />
+              <span class="year">. 2023</span>
+            </p>
+          </div>
+        </div>
 
+        <a href="https://www.tecnocasa.es" target="_blank">
+          <img src={Tecnocasa} alt="tecnocasa" className="tecnocasa" />
+        </a>
+      </div>
 
+      <div className="section">
+        <div className="proyecto">
+          <div className="contAbout-texto">
+            <p>
+              <span class="job-role">. Full Stack Web Developer</span>
+              <br />
+              <span class="company-name">. Henry</span>
+              <br />
+              <span class="year">. 2023</span>
+            </p>
+          </div>
+        </div>
 
-return (
-    <div className="trabajo-container"></div>
-)
+        <a href="https://www.tecnocasa.es" target="_blank">
+          <img src={Tecnocasa} alt="tecnocasa" className="tecnocasa" />
+        </a>
+      </div>
 
+      <div className="section">
+        <div className="proyecto">
+          <div className="contAbout-texto">
+            <p>
+              <span class="job-role">. Full Stack Web Developer</span>
+              <br />
+              <span class="company-name">. Henry</span>
+              <br />
+              <span class="year">. 2023</span>
+            </p>
+          </div>
+        </div>
+
+        <a href="https://www.tecnocasa.es" target="_blank">
+          <img src={Tecnocasa} alt="tecnocasa" className="tecnocasa" />
+        </a>
+      </div>
+
+      <div className="section">
+        <div className="proyecto">
+          <div className="contAbout-texto">
+            <p>
+              <span class="job-role">. Full Stack Web Developer</span>
+              <br />
+              <span class="company-name">. Henry</span>
+              <br />
+              <span class="year">. 2023</span>
+            </p>
+          </div>
+        </div>
+
+        <a href="https://www.tecnocasa.es" target="_blank">
+          <img src={Tecnocasa} alt="tecnocasa" className="tecnocasa" />
+        </a>
+      </div>
+
+      <div className="section">
+        <div className="proyecto">
+          <div className="contAbout-texto">
+            <p>
+              <span class="job-role">. Full Stack Web Developer</span>
+              <br />
+              <span class="company-name">. Henry</span>
+              <br />
+              <span class="year">. 2023</span>
+            </p>
+          </div>
+        </div>
+
+        <a href="https://www.tecnocasa.es" target="_blank">
+          <img src={Tecnocasa} alt="tecnocasa" className="tecnocasa" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default trabajo;
