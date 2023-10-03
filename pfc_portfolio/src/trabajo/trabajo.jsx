ba17b30676f63563b991c75372eb5c1cc84173e4
PedroCavataio <pedrocavataio@gmail.com>
2023-10-03 18:24:01 -0300

    trabajo imagenes ok

--- pfc_portfolio/src/trabajo/trabajo.jsx
+++ pfc_portfolio/src/trabajo/trabajo.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./trabajo.styles.css";
-import Umet from "../assets/umet.png";
+import Countries from "../assets/countriesDeploy.png";
+import Rick from "../assets/rickDeploy.png";
+import Casting from "../assets/castingAppDeploy.png";
+import Control from "../assets/controlDeploy.png";
 
 const trabajo = () => {
 
@@ -13,25 +16,32 @@ const trabajo = () => {
         
         <div className="deploy">
           <a href="https://www.umet.edu.ar" target="_blank">
-            <img src={Umet} alt="umet" className="umet" />
+          <div className="image-container left">
+            <img src={Countries} alt="countries" className="countries" />
+            </div>
           </a>
         </div>
       </div>
-     
+
+  
       <div className="trabajo-container">
        
-        <div className="deploy">
+      <div className="deploy">
           <a href="https://www.umet.edu.ar" target="_blank">
-            <img src={Umet} alt="umet" className="umet" />
+            <div className="image-container">
+            <img src={Rick} alt="rick" className="rick" />
+            </div>
           </a>
         </div>
       </div>
       
       <div className="trabajo-container">
        
-        <div className="deploy">
-          <a href="https://www.umet.edu.ar" target="_blank">
-            <img src={Umet} alt="umet" className="umet" />
+      <div className="deploy">
+          <a href="https://deploy-sprint-2-frontend.onrender.com/" target="_blank">
+          <div className="image-container left">
+            <img src={Casting} alt="castingAppDeploy" className="castingAppDeploy" />
+            </div>
           </a>
         </div>
       </div>
@@ -39,9 +49,11 @@ const trabajo = () => {
       <div className="trabajo-container">
        
 
-        <div className="deploy">
+      <div className="deploy">
           <a href="https://www.umet.edu.ar" target="_blank">
-            <img src={Umet} alt="umet" className="umet" />
+          <div className="image-container">
+            <img src={Control} alt="control" className="control" />
+            </div>
           </a>
         </div>
       </div>
