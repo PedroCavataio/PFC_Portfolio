791fdfe394f6fa49ecbb84dd7e0b07a594a0350d
PedroCavataio <pedrocavataio@gmail.com>
2023-10-09 00:02:02 -0300

    trabajo

--- pfc_portfolio/src/trabajo/trabajo.jsx
+++ pfc_portfolio/src/trabajo/trabajo.jsx
@@ -1,3 +1,4 @@
+/* trabajo.jsx */
 import React from "react";
 import "./trabajo.styles.css";
 import Countries from "../assets/countriesDeploy.png";
@@ -13,103 +14,23 @@ import Node from "../assets/node.png";
 import Js from "../assets/js.png";
 import Express from "../assets/express.png";
 import Postgress from "../assets/postgresqlIcono.png";
+import Excel from "../assets/excel.png";
+import Bootstrap from "../assets/bootstrap.png";
+import FormsPree from "../assets/formsPree.png";
+import NodeMailer from "../assets/nodeMailer.png";
 
 const trabajo = () => {
   return (
     <>
       <div className="background">
         <div className="containerJob">
-
           <div className="trabajo-container">
             <div className="deploy">
               <a href="https://www.umet.edu.ar" target="_blank">
                 <div className="image-container left">
                   <img src={Countries} alt="countries" className="countries" />
-
-                  <div className="skill-container">
-                    <div className="skill-group">                      
-                      <img src={Redux} alt="Redux" className="redux" />
-                      <img src={ReactIcono} alt="React" className="react" />
-                      <img src={Js} alt="js" className="js" />
-                    </div>
-                    <div className="skill-group">                                      
-                      <img src={Css} alt="css" className="css" />
-                      <img src={Html} alt="html" className="html" />
-                      <img src={Sequelize} alt="sequelize" className="sequelize" />
-                    </div>
-                    <div className="skill-group">
-                      <img src={Postgress} alt="postgress" className="postgress" />
-                      <img src={Node} alt="node" className="node" />
-                      <img src={Express} alt="express" className="express" />
-                    </div>  
-                  </div>
-
-                </div>                
-              </a>              
-            </div>
-          </div>
-
-          <div className="trabajo-container">
-            <div className="deploy">
-              <a href="https://www.umet.edu.ar" target="_blank">
-                <div className="image-container">
-                  <img src={Rick} alt="rick" className="rick" /> 
-
-                  <div className="skill-container">
-                    <div className="skill-group">                      
-                      <img src={Redux} alt="Redux" className="redux" />
-                      <img src={ReactIcono} alt="React" className="react" />
-                      <img src={Js} alt="js" className="js" />
-                    </div>
-                    <div className="skill-group">                                      
-                      <img src={Css} alt="css" className="css" />
-                      <img src={Html} alt="html" className="html" />
-                      <img src={Sequelize} alt="sequelize" className="sequelize" />
-                    </div>
-                    <div className="skill-group">
-                      <img src={Postgress} alt="postgress" className="postgress" />
-                      <img src={Node} alt="node" className="node" />
-                      <img src={Express} alt="express" className="express" />
-                    </div>  
-                  </div>
-
-                  </div>
-                  </a>              
-                  
-              </div>
-           </div>
-          
-
-          <div className="trabajo-container">
-            <div className="deploy">
-              <a href="https://deploy-sprint-2-frontend.onrender.com/"
-                target="_blank">
-                <div className="image-container left">
-                  <img src={Casting}
-                    alt="castingAppDeploy"
-                    className="castingAppDeploy"
-                  /> 
-
-                <div className="skill-container">
-                    <div className="skill-group">                      
-                      <img src={Redux} alt="Redux" className="redux" />
-                      <img src={ReactIcono} alt="React" className="react" />
-                      <img src={Js} alt="js" className="js" />
-                    </div>
-                    <div className="skill-group">                                      
-                      <img src={Css} alt="css" className="css" />
-                      <img src={Html} alt="html" className="html" />
-                      <img src={Sequelize} alt="sequelize" className="sequelize" />
-                    </div>
-                    <div className="skill-group">
-                      <img src={Postgress} alt="postgress" className="postgress" />
-                      <img src={Node} alt="node" className="node" />
-                      <img src={Express} alt="express" className="express" />
-                    </div>  
-                  </div>
-                  </div>
+                </div>
               </a>
-              
             </div>
           </div>
 
@@ -117,29 +38,41 @@ const trabajo = () => {
             <div className="deploy">
               <a href="https://www.umet.edu.ar" target="_blank">
                 <div className="image-container">
-                  <img src={Control} alt="control" className="control" /> 
-
-                <div className="skill-container">
-                    <div className="skill-group">                      
-                      <img src={Redux} alt="Redux" className="redux" />
-                      <img src={ReactIcono} alt="React" className="react" />
-                      <img src={Js} alt="js" className="js" />
-                    </div>
-                    <div className="skill-group">                                      
-                      <img src={Css} alt="css" className="css" />
-                      <img src={Html} alt="html" className="html" />
-                      <img src={Sequelize} alt="sequelize" className="sequelize" />
-                    </div>
-                    <div className="skill-group">
-                      <img src={Postgress} alt="postgress" className="postgress" />
-                      <img src={Node} alt="node" className="node" />
-                      <img src={Express} alt="express" className="express" />
-                    </div>  
-                  </div>
-                  </div>
-              </a>              
+                  <img src={Rick} alt="rick" className="rick" />
+                </div>
+              </a>
             </div>
-          </div>          
+          </div>
+
+          <div className="trabajo-container">
+            <div className="deploy">
+              <a href="https://deploy-sprint-2-frontend.onrender.com/" target="_blank">
+                <div className="image-container left">
+                  <img src={Casting} alt="castingAppDeploy" className="castingAppDeploy" />
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div className="trabajo-container">
+            <div className="deploy">
+              <a href="https://www.umet.edu.ar" target="_blank">
+                <div className="image-container">
+                  <img src={Control} alt="control" className="control" />
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div className="trabajo-container">
+            <div className="deploy">
+              <a href="https://deploy-sprint-2-frontend.onrender.com/" target="_blank">
+                <div className="image-container left">
+                  <img src={Casting} alt="castingAppDeploy" className="castingAppDeploy" />
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
